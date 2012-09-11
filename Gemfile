9ac8cd63f149ea6ef369cd07cf7a71e182146b05
Nathan Martin <njmartin10@gmail.com>
2012-09-11 09:27:05 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+#in this tutorial we’ll include explicit version numbers known to work
+#http://ruby.railstutorial.org/chapters/beginning#sec-up_and_running
+
+gem 'rails', '3.2.8'
+
+group :development do
+  gem 'sqlite3', '1.3.5'
+end
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+
+  gem 'uglifier', '1.2.3'
+end
+
+gem 'jquery-rails', '2.0.2'
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5aaf90686f0c91d79fadcc05d35d91b4b0a3d6230a7e2a91d6e1ddfd031e8059d8ae417cc55d85c27ff2fcda3049b175ee00c2607e322b6e3ea612be38407e3b'
